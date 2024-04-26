c7a81ff34f120633e7d0b8b1bf48d86d87dc6f20
Tejas More <tejasmore660@gmail.com>
2024-04-26 19:42:22 +0530

    Added JS inside DOMContentLoaded event listener

--- script.js
+++ script.js
@@ -1,108 +1,105 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let YearOP = document.getElementById("displayYear");
+    let MonthsOP = document.getElementById("displayMonths");
+    let DaysOP = document.getElementById("displayDays");
 
-let YearOP = document.getElementById("displayYear");
-let MonthsOP = document.getElementById("displayMonths");
-let DaysOP = document.getElementById("displayDays");
+    let submit = document.querySelector(".submitBtn");
 
-let submit = document.querySelector(".submitBtn");
+    submit.addEventListener("click", () => {
+        validateInputs();
+    });
+
+    const validateInputs = () => {
+        let hasError = false;
+
+        let inpDay = parseInt(document.querySelector(".inpday").value.trim());
+        let inpMonths = parseInt(document.querySelector(".inpmonth").value.trim());
+        let inpuYears = parseInt(document.querySelector(".inpyear").value.trim());
+
+        if (isNaN(inpDay) || inpDay === "") {
+            errorCall(document.querySelector(".inpday"), "Day cannot be empty");
+            hasError = true;
+        } 
+        else if (inpDay > 31 || inpDay <= 0) {
+            errorCall(document.querySelector(".inpday"), "Invalid Day");
+            hasError = true;
+        }
+        else{
+            clearError(document.querySelector(".inpday"));
+        }
+
+        if (isNaN(inpMonths) || inpMonths === "") {
+            errorCall(document.querySelector(".inpmonth"), "Month cannot be empty");
+            hasError = true;
+        }
+         else if (inpMonths > 12 || inpMonths <= 0) {
+            errorCall(document.querySelector(".inpmonth"), "Invalid Month");
+            hasError = true;
+        }
+        else{
+            clearError(document.querySelector(".inpmonth"));
+        }
 
 
-submit.addEventListener("click", () => {
-    validateInputs();
+        if (isNaN(inpuYears) || inpuYears === "") {
+            errorCall(document.querySelector(".inpyear"), "Year cannot be empty");
+            hasError = true;
+        } 
+        else if (inpuYears <= 0) {
+            errorCall(document.querySelector(".inpyear"), "Year can't be negative");
+        }
+        else if(inpuYears.toString().length < 4 ) {
+            errorCall(document.querySelector(".inpyear"), "Invalid Year");
+        }
+        else{
+            clearError(document.querySelector(".inpyear"));
+        }
+
+        // Call displayOutput function only if no validation errors occurred
+        if (!hasError) {
+            displayOutput(inpDay, inpMonths, inpuYears);
+        }
+    }
+
+    const errorCall = (input , errorMsg) => {
+        const parent  = input.parentNode;
+        const error  = parent.querySelector(".emptyError"); 
+        error.innerText = errorMsg;
+        parent.className = "inputs failure"
+    }
+
+    const clearError = (input) => {
+        const parent = input.parentNode;
+        const error = parent.querySelector(".emptyError")
+        if(error) {
+            error.innerText = "";
+            parent.className = "inputs";
+        }
+    }
+
+    const displayOutput = (inpDay, inpMonths, inpuYears) => {
+        let currentDate = new Date();
+        let currentYear = currentDate.getFullYear();
+        let currentMonth = currentDate.getMonth() + 1; // Month is zero-based, so we add 1
+        let currentDay = currentDate.getDate();
+
+        let ageYears = currentYear - inpuYears;
+        let ageMonths = currentMonth - inpMonths;
+        let ageDays = currentDay - inpDay;
+
+        if (currentMonth < inpMonths || (currentMonth === inpMonths && currentDay < inpDay)) {
+            ageYears--;
+            ageMonths += 12;
+        }
+
+        if (ageDays < 0) {
+            ageMonths--;
+            let previousMonthDays = new Date(currentYear, currentMonth - 1, 0).getDate();
+            ageDays += previousMonthDays;
+        }
+
+        YearOP.innerText = ageYears;
+        MonthsOP.innerText =  ageMonths;
+        DaysOP.innerText = ageDays;
+    }
 });
-
-
-const validateInputs = () => {
-    let hasError = false;
-
-    let inpDay = parseInt(document.querySelector(".inpday").value.trim());
-    let inpMonths = parseInt(document.querySelector(".inpmonth").value.trim());
-    let inpuYears = parseInt(document.querySelector(".inpyear").value.trim());
-
-    if (inpDay === "") {
-        errorCall(document.querySelector(".inpday"), "Day cannot be empty");
-        hasError = true;
-    } 
-    else if (inpDay > 32 || inpDay <= 0) {
-        errorCall(document.querySelector(".inpday"), "Invalid Day");
-        hasError = true;
-    }
-    else{
-        clearError(document.querySelector(".inpday"));
-    }
-
-    if (inpMonths === "") {
-        errorCall(document.querySelector(".inpmonth"), "Month cannot be empty");
-        hasError = true;
-    }
-     else if (inpMonths > 12 || inpMonths <= 0) {
-        errorCall(document.querySelector(".inpmonth"), "Invalid Month");
-        hasError = true;
-    }
-    else{
-        clearError(document.querySelector(".inpmonth"));
-    }
-
-
-    if (inpuYears === "") {
-        errorCall(document.querySelector(".inpyear"), "Year cannot be empty");
-        hasError = true;
-    } 
-    else if (inpuYears <= 0) {
-        errorCall(document.querySelector(".inpyear"), "Year cann't be negative");
-    }
-    else if(inpuYears.length < 4 ) {
-        errorCall(document.querySelector(".inpyear"), "Invalid Year");
-    }
-    else{
-        clearError(document.querySelector(".inpyear"));
-    }
-
-    // Call displayOutput function only if no validation errors occurred
-    if (!hasError) {
-    displayOutput(inpDay, inpMonths, inpuYears);
-    }
-
-}
-
-const errorCall = (input , errorMsg) => {
-    const parent  = input.parentNode;
-    const error  = parent.querySelector(".emptyError"); 
-    error.innerText = errorMsg;
-    parent.className = "inputs failure"
-}
-
-const clearError = (input) => {
-    const parent = input.parentNode;
-    const error = parent.querySelector(".emptyError")
-    if(error) {
-        error.innerText = "";
-        parent.className = "inputs";
-    }
-}
-
-const displayOutput = (inpDay, inpMonths, inpuYears) => {
-    let currentDate = new Date();
-    let currentYear = currentDate.getFullYear();
-    let currentMonth = currentDate.getMonth() + 1; // Month is zero-based, so we add 1
-    let currentDay = currentDate.getDate();
-
-    let ageYears = currentYear - inpuYears;
-    let ageMonths = currentMonth - inpMonths;
-    let ageDays = currentDay - inpDay;
-
-    if (currentMonth < inpMonths || (currentMonth === inpMonths && currentDay < inpDay)) {
-        ageYears--;
-        ageMonths += 12;
-    }
-
-    if (ageDays < 0) {
-        ageMonths--;
-        let previousMonthDays = new Date(currentYear, currentMonth - 1, 0).getDate();
-        ageDays += previousMonthDays;
-    }
-
-    YearOP.innerText = ageYears;
-    MonthsOP.innerText =  ageMonths;
-    DaysOP.innerText = ageDays;
-}
-
